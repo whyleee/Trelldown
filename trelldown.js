80c1b6798539b12a491f6fe8c2654848d28c970f
Pavel Nezhencev <whyleee@gmail.com>
2013-11-07 02:44:41 +0200

    Fixed chart layout, removed weekends, integrated with diff point groups, backlog and sprints (hardcoded)

--- trelldown.js
+++ trelldown.js
@@ -52,30 +52,88 @@
 		// burndown
 		var lastDoc = null;
 		
+		// now works only with 'NBT' board (hardcoded)
+		var boardName = $('.board-name').text().trim();
+		if (boardName != 'NBT') {
+			return;
+		}
+		
 		$('.board-header-btns.right').prepend(
 			'<a href="#" class="board-header-btn td-open-burndown"><span class="text">Burndown</span></a>'
 		);
 		$('.td-open-burndown').on('click', function(){
-			$.modal('<div id="basic-modal-content" style="background: white; padding: 20px"><h2>Trello Burndown</h2><p style="width: 800px"></p><div id="td-burndown-chart"></div></div>');
+			$.modal(
+				('<div id="basic-modal-content" style="background: white; padding: 20px">' +
+					'<a class="simplemodal-close" title="Close" style="position: absolute; top: 5px; right: 8px; color: black">x</a>' +
+					'<h1>{title}</h1><hr/>' +
+					'<div id="td-burndown-chart" style="width: 100%; height: 500px"></div>' +
+				'</div>')
+				.replace('{title}', 'Trello Burndown'), {
+					containerCss: {
+						background: '#e3e3e3',
+						paddingRight: '26px',
+						paddingTop: '20px',
+						width: '1000px',
+						height: '620px'
+					}
+				}
+			);
 
-			$.getJSON('http://127.0.0.1:5984/trello_test/aa8285e448290a4a84995258e402fcbe', function(data){
+			$.getJSON('http://couch.lab409.com:5984/trelldown/9a20e8758fac4cd6e5e7a6acff030435', function(data){
 				lastDoc = data;
 				updateDb();
 
-				var days = lastDoc.dates.map(function(e) {var dd = new Date(e); return dd.getDate() + '-' + (dd.getMonth() + 1) + '-' + dd.getFullYear();});
 				var hours = lastDoc.hours_left;
+				//var days = lastDoc.dates.map(function(e) {var dd = new Date(e); return dd.getDate() + '-' + (dd.getMonth() + 1) + '-' + dd.getFullYear();});
+				var days = lastDoc.dates.map(function(e) {return new Date(e);});
+				var sprintLength = 10; // days
+				var daysLeftCount = sprintLength - lastDoc.dates.length + 1;
+				
+				if (daysLeftCount > 0){
+					var lastDay = new Date(lastDoc.dates[lastDoc.dates.length - 1]);
+					for (var i = 1; i <= daysLeftCount; ++i){
+						var nextDay = new Date();
+						nextDay.setDate(lastDay.getDate() + i);
+
+						if (nextDay.getDay() < 2){ // Sundary or Monday
+							++daysLeftCount;
+							continue;
+						}
+
+						var nextDayText = nextDay.getDate() + '-' + (nextDay.getMonth() + 1) + '-' + nextDay.getFullYear();
+						//days.push(nextDayText);
+						days.push(nextDay);
+						hours.push(null);
+					}
+				}
 
 				$('#td-burndown-chart').highcharts({
 		            title: {
-		                text: 'CPH Team, Hay'
+		                text: 'CPH Team, NBT'
+		            },
+		            credits: {
+		                enabled: false
+		            },
+		            legend: {
+		                enabled: false
 		            },
 		            xAxis: {
-		                categories: days
+		                categories: days,
+		                gridLineWidth: 1,
+		                type: 'datetime',
+                        tickmarkPlacement: 'on',
+                        labels: {
+            	            formatter: function() {
+				                return Highcharts.dateFormat('%a, %b %d', this.value);
+				            }
+                        }
 		            },
 		            yAxis: {
 		                title: {
 		                    text: 'Hours left'
 		                },
+		                max: hours.length > 0 ? hours[0] + 5 : null,
+		                min: 0,
 		                plotLines: [{
 		                    value: 0,
 		                    width: 1,
@@ -84,12 +142,6 @@
 		            },
 		            tooltip: {
 		                valueSuffix: 'h'
-		            },
-		            legend: {
-		                layout: 'vertical',
-		                align: 'right',
-		                verticalAlign: 'middle',
-		                borderWidth: 0
 		            },
 		            series: [{
 		                name: 'CPH',
@@ -103,10 +155,27 @@
 		function updateDb(){
 			var _lists = ModelCache._cache.List;
 			var _cards = ModelCache._cache.Card;
-			var totalLeftText = $('.list-title:contains("Todo"), .list-title:contains("To Do")').find('.list-total .points').text();
-			var totalLeft = totalLeftText ? parseInt(totalLeftText) : 0;
+			var totalPoints = getSumPoints($('#board-header .list-total'));
+			var donePoints = getSumPoints($('.list-title:contains("Done") .list-total'));
+			var totalLeft = totalPoints - donePoints;
+			// var totalLeftText = $('.list-title:contains("Todo"), .list-title:contains("To Do")').find('.list-total .points').text();
+			// var totalLeft = totalLeftText ? parseInt(totalLeftText) : 0;
 
+			var sprintOnly = true;
+			if (sprintOnly) {
+				var backlogPoints = getSumPoints($('.list-title:contains("Backlog") .list-total'));
+				var sprintPoints = totalPoints - backlogPoints;
+				totalLeft = sprintPoints - donePoints;
+			}
 
+			function getSumPoints($totals){
+				var sumPoints = 0;
+				$totals.find('span[class^="points-"],span[class*=" points-"]').each(function(){
+					var pointsText = $(this).text();
+					sumPoints += pointsText ? parseInt(pointsText) : 0;
+				});
+				return sumPoints;
+			}
 			// //for each list
 			// for(var listID in _lists){
 			// 	if(!_lists.hasOwnProperty(listID)){ continue; } //skip if not a list
@@ -129,22 +198,35 @@
 			// 	}
 			// }
 
-			var today = new Date().toDateString();
-			var todaysInDoc = lastDoc.dates.filter(function(e){return new Date(e).toDateString() == today});
+			var today = new Date();
+			var tomorrow = new Date();
+			tomorrow.setDate(today.getDate() + 1);
+
+			var tomorrowString = tomorrow.toDateString();
+			var tomorrowsInDoc = lastDoc.dates.filter(function(e){return new Date(e).toDateString() == tomorrowString});
 			var lastHours = lastDoc.hours_left[lastDoc.hours_left.length - 1];
 
-			if (lastHours != totalLeft || todaysInDoc.length == 0) {
+			if (lastHours != totalLeft || tomorrowsInDoc.length == 0) {
+				var weekend = tomorrow.getDay() < 2;
 				// if nothing were tracked today - add new record
-				if (todaysInDoc.length == 0) {
-					lastDoc.dates.push(new Date().getTime());
+				if (!weekend && tomorrowsInDoc.length == 0) {
+					// if this is the first day - add yesterday also
+					if (lastDoc.dates.length == 0) {
+						lastDoc.dates.push(today.getTime());
+						lastDoc.hours_left.push(totalLeft);
+					}
+					lastDoc.dates.push(tomorrow.getTime());
 					lastDoc.hours_left.push(totalLeft);
 				} else { // else update the last record
-					lastDoc.dates[lastDoc.dates.length - 1] = new Date().getTime();
+					if (lastDoc.dates.length == 0) { // it looks like today is weekend and no work done yet
+						return;
+					}
+					lastDoc.dates[lastDoc.dates.length - 1] = tomorrow.getTime();
 					lastDoc.hours_left[lastDoc.hours_left.length - 1] = totalLeft;
 				}
 
 				$.ajax({
-					url: 'http://127.0.0.1:5984/trello_test/aa8285e448290a4a84995258e402fcbe',
+					url: 'http://couch.lab409.com:5984/trelldown/9a20e8758fac4cd6e5e7a6acff030435',
 					type: 'PUT',
 					data: JSON.stringify(lastDoc),
 					success: function(){
